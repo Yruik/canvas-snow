2b81fc1acdf881e222cf4b99f764db399ad927cd
Yruik <dream1996vip@live.com>
2018-12-28 13:14:50 +0800

    添加基本代码

--- js/canvas-snow.js
+++ js/canvas-snow.js
@@ -7,7 +7,20 @@ Canvas.prototype.Init = function(){
     document.styleSheets[0].addRule("*","margin:0px;padding:0px;");
     // 创建画布
     const canvas = document.createElement("canvas");
+    // 设置画布样式
     canvas.width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     canvas.height =  window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    canvas.style.backgroundColor = "grey";
+    canvas.style.position = "absolute";
+    canvas.style.zIndex = "-1";
+    document.body.appendChild(canvas);
+    this.canvas = canvas;
+    this.ctx = canvas.getContext('2d');
 }
 var canvas = new Canvas()
+canvas.Init();
+canvas.ctx.beginPath();
+canvas.ctx.font = "50px 宋体";
+canvas.ctx.fillStyle = "white";
+canvas.ctx.fillText("❄",50,500);
+canvas.ctx.closePath();
